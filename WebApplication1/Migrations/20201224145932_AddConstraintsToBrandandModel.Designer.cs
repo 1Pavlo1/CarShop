df787f8a1f174193d6e697c08e33dc59f2ec0a3e
dp <fandorine77@gmail.com>
2020-12-24 17:04:11 +0200

    Add constraints to properties in Model and Brand.

--- WebApplication1/Migrations/20201224145932_AddConstraintsToBrandandModel.Designer.cs
+++ WebApplication1/Migrations/20201224145932_AddConstraintsToBrandandModel.Designer.cs
@@ -3,14 +3,16 @@ using CarShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarShop.Migrations
 {
     [DbContext(typeof(CarShopContext))]
-    partial class CarShopContextModelSnapshot : ModelSnapshot
+    [Migration("20201224145932_AddConstraintsToBrandandModel")]
+    partial class AddConstraintsToBrandandModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebApplication1/Models/Model.cs
+++ WebApplication1/Models/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace CarShop.Models
     public class Model
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
         public Brand Brand { get; set; }
         public int BrandId { get; set; }
