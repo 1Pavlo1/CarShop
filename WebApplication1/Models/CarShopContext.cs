57551936a537d70c200f7effd7be168f588d6986
dp <fandorine77@gmail.com>
2020-12-24 16:49:58 +0200

    Add Brand and Model models and create Db.

---
+++ WebApplication1/Models/CarShopContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Models
+{
+    public class CarShopContext : DbContext
+    {
+        public CarShopContext(DbContextOptions<CarShopContext> options) :
+            base(options)
+        {
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Model> Models { get; set; }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WebApplication1
+namespace CarShop
 {
     public class Startup
     {
@@ -24,6 +26,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<CarShopContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
