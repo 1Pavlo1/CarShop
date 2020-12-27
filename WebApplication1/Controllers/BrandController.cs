f26d3501f9f613c1383d09f240992793754ec221
dp <fandorine77@gmail.com>
2020-12-27 20:57:30 +0200

    Add functionality for creating and showing brand of cars.

---
+++ WebApplication1/Controllers/BrandController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CarShop.Models;
+
+namespace CarShop.Controllers
+{
+    public class BrandController : Controller
+    {
+        private CarShopContext _dataBase;
+
+        public BrandController(CarShopContext dataBase)
+        {
+            _dataBase = dataBase;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Brand brand)
+        {
+            if(ModelState.IsValid)
+            {
+                _dataBase.Add(brand);
+                _dataBase.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+        public IActionResult Index()
+        {
+            return View(_dataBase.Brands.ToList());
+        }
+    }
+}
