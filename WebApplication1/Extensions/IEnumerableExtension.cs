220f48e9095d58e6fccf5a4ee77ff347277dbe67
dp <fandorine77@gmail.com>
2021-01-01 12:23:12 +0200

    Add extension methods.

---
+++ WebApplication1/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items)
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            SelectListItem sli = new SelectListItem
+            {
+                Text = "-----Select-----",
+                Value = "0"
+            };
+            list.Add(sli);
+
+            foreach (var item in items)
+            {
+                sli = new SelectListItem
+                {
+                    Text = item.GetPropertyValue("Name"),
+                    Value = item.GetPropertyValue("Id")
+                };
+                list.Add(sli);
+            }
+            return list;
+        }
+    }
+}
